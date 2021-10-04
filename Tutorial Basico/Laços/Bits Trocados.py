df21fc4d51d67b858d6a4d226bb9368abf14dfe1
ReiDoFuscaBR <91832540+Izak76@users.noreply.github.com>
2021-10-04 15:38:52 -0300

    Solução comentada

--- Tutorial Basico/Laços/Bits Trocados.py
+++ Tutorial Basico/Laços/Bits Trocados.py
@@ -1,14 +1,14 @@
-n = 1
+n = 1 #Contador do teste
 while True:
-    v = int(input())
-    if not v:
-        break
+    v = int(input()) #Valor em bits
+    if not v: #Se v = 0 (Valor falso)...
+        break #encerra o loop
         
     print("Teste", n)
-    for x in (50, 10, 5, 1):
-        nc = v//x
-        v -= nc*x
-        print(nc, end=" ")
+    for x in (50, 10, 5, 1): #Repete o mesmo processo com todas os valores de cédulas
+        nc = v//x #Número de cédulas de x de modo que x*nc <= v
+        v -= nc*x #Remove de v todo o valor já contabilizado
+        print(nc, end=" ") #Imprime o número de cédulas
 
-    print("\n")
-    n += 1
+    print("\n") #Pula uma linha para o próximo caso de teste
+    n += 1 #Incrementa o caso de teste
