1fb2badcf7673a7d5660d75301519d2fe04d5af7
ReiDoFuscaBR <91832540+Izak76@users.noreply.github.com>
2021-10-07 17:37:49 -0300

    Solução não comentada

--- Tutorial Basico/Vetores/Notas.cpp
+++ Tutorial Basico/Vetores/Notas.cpp
@@ -1,4 +1,4 @@
-//Este algoritmo é uma "tradução" da versão em python. Ela está disponível em: 
+//Este algoritmo é uma "tradução" da versão em python. Ela está disponível em: https://bit.ly/3BoRcKr
 /*Por mais estranho que pareça, mesmo que ambas as versões (Python e C++)
 funcionem praticamente da mesma forma, somente a versão em C++ (esta aqui)
 é aceita no sistema de correção. A versão em Python dá "Resposta errada".
