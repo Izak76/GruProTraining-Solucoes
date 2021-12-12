594a410b935d3dc4e3c23dcec79c232f224ef7f6
Izak A <izakalvesgama@outlook.com>
2021-12-12 15:55:13 -0300

    Solução comentada

--- Tutorial Basico/Vetores/Fila.py
+++ Tutorial Basico/Vetores/Fila.py
@@ -1,15 +1,8 @@
-#Ignora a 1ª entrada, pois não será necessária
-input()
-#Cria um dicionário com todas as pessoas da fila, com valores inicialmente verdadeiros
-fila = dict.fromkeys(input().split(), True)
-#Ignora a 3ª entrada, pois não será necessária
-input()
-#Precorre a lista formada por todas as pessoas que sairam
-for x in input().split():
-	#Todas as pessoas que sairam da fila agora ganham valor falso
-	fila[x] = False
+input()  #Ignora a 1ª entrada, pois não será necessária
+fila = dict.fromkeys(input().split(), True)  #Cria um dicionário com todas as pessoas da fila, com valores inicialmente verdadeiros
+input()  #Ignora a 3ª entrada, pois não será necessária
+for x in input().split():  #Precorre a lista formada por todas as pessoas que sairam
+	fila[x] = False  #Todas as pessoas que sairam da fila agora ganham valor falso
 
-#Filtra a fila, permanecendo apenas as pessoas que ainda possuem valor verdadeiro
-nfila = filter(lambda y: fila[y], fila)
-#Imprime o resultado do filtro
-print(" ".join(nfila))
+nfila = filter(lambda y: fila[y], fila)  #Filtra a fila, permanecendo apenas as pessoas que ainda possuem valor verdadeiro
+print(" ".join(nfila))  #Imprime o resultado do filtro
