67c5e826b0bf7a9b2414a8f6918efa0f3e26cbeb
Izak A <izakalvesgama@outlook.com>
2022-02-11 23:21:31 -0300

    Solução não comentada

--- Tutorial Basico/Recursão/Rumo aos 9s.py
+++ Tutorial Basico/Recursão/Rumo aos 9s.py
@@ -1,14 +1,14 @@
 somalg = lambda n: sum(map(int, str(n)))
 for x in iter(lambda: int(input()), 0):
-	xback = x
-	
-	if x%9:
-		print(f"{x} is not a multiple of 9.")
-		continue
+    xback = x
+    
+    if x%9:
+        print(f"{x} is not a multiple of 9.")
+        continue
 
-	grau = 1
-	for y in iter(lambda: somalg(x), 9):
-		grau += 1
-		x = y
+    grau = 1
+    for y in iter(lambda: somalg(x), 9):
+        grau += 1
+        x = y
 
-	print(f"{xback} is a multiple of 9 and has 9-degree {grau}.")
+    print(f"{xback} is a multiple of 9 and has 9-degree {grau}.")
