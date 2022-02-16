2a6a0eb56ff680e2a5624d6766afabefb43ac416
Izak A <izakalvesgama@outlook.com>
2022-02-16 13:33:57 -0300

    Solução não comentada

--- Tutorial Basico/Vetores/Notas.cpp
+++ Tutorial Basico/Vetores/Notas.cpp
@@ -1,30 +1,27 @@
-//Este algoritmo é uma "tradução" da versão em python. Ela está disponível em: https://bit.ly/3DkNjXx
-/*Por mais estranho que pareça, mesmo que ambas as versões (Python e C++)
-funcionem praticamente da mesma forma, somente a versão em C++ (esta aqui)
-é aceita no sistema de correção. A versão em Python dá "Resposta errada".
-Por qual motivo? Bom, eu não sei e não entendo o por quê.*/
-
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+#include <map>
+
 using namespace std;
 
+bool comp(pair<short, short> x, pair<short, short> y){
+    if (x.second == y.second){
+        return x.first < y.first;
+    }
+
+    return x.second < y.second;
+}
+
 int main(){
-    short nn;
-    cin >> nn;
-    short n[nn];
-    for(short x=0; x<nn; x++){
-        cin >> n[x];
+    short  n, nt;
+    cin >> n;
+
+    map<short, short> notas;
+    for(short i=0; i<n; i++){
+        cin >> nt;
+        notas.emplace(nt, 0);
+        notas[nt]++;
     }
-    short maior=0, cmaior=0, ct; //O maior número e a quantidade dele no array, respectivamente
-    for (short x: n){
-        ct = count(n, n+nn, x);
-        if (ct > cmaior){
-            maior = x;
-            cmaior = ct;
-        }
-        else if (ct == cmaior && x > maior){
-            maior = x;
-        }
-    }
-    cout << maior << endl;
+
+    cout << (*max_element(notas.begin(), notas.end(), comp)).first << endl;
 }
--- Tutorial Basico/Vetores/Notas.py
+++ Tutorial Basico/Vetores/Notas.py
@@ -1,15 +1,13 @@
+#Esta solução foi corrigida e aceita utilizando os casos de teste originais da OBI,
+#pois a solução em Python 3 por motivos desconhecidos dá "Resposta errada" no SPOJ.
+
+#Os casos de teste podem ser baixados em: https://olimpiada.ic.unicamp.br/static/extras/obi2014/gabaritos/2014f2p2_notas.zip
+#Para uma solução que passe pelo SPOJ, aqui está a versão em C++: 
 input()
-n = tuple(map(int, input().split()))
+cont = {}
 
-maior, cmaior = 0, 0
-for x in set(n):
-    ct = n.count(x)
-    if ct > cmaior:
-        maior = x
-        cmaior = ct
+for x in map(int, input().split()):
+    cont.setdefault(x, 0)
+    cont[x] += 1
 
-    elif ct == maior and x > maior:
-        maior = x
-
-print(maior)
-        
+print(max(cont, key=lambda x: (cont[x], x)))
