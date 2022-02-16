dec9583617363ca7a8a630cce359edf9e8f53b9d
Izak A <izakalvesgama@outlook.com>
2022-02-16 13:37:04 -0300

    Solução não comentada

--- Tutorial Basico/Vetores/Notas.py
+++ Tutorial Basico/Vetores/Notas.py
@@ -2,7 +2,8 @@
 #pois a solução em Python 3 por motivos desconhecidos dá "Resposta errada" no SPOJ.
 
 #Os casos de teste podem ser baixados em: https://olimpiada.ic.unicamp.br/static/extras/obi2014/gabaritos/2014f2p2_notas.zip
-#Para uma solução que passe pelo SPOJ, aqui está a versão em C++: 
+#Para uma solução que passe pelo SPOJ, aqui está a versão em C++: https://bit.ly/3GO7ezy
+
 input()
 cont = {}
 
